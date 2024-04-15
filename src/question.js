1649c50affa79741eaffcf6a13bbe0c6ce9b1287
Ignacio Monzón Acosta <ignaciomonzona@gmail.com>
2024-04-15 17:39:21 +0200

    Día 1 terminado a falta de checkAnswer method

--- src/question.js
+++ src/question.js
@@ -1,14 +1,14 @@
 class Question {
-    // 1. constructor (text, choices, answer, difficulty)
-    constructor(text, choices, answer, difficulty) {
-        this.text = text
-        this.choices = choices
-        this.answer = answer
-        this.difficulty = difficulty
-    }
+  // 1. constructor (text, choices, answer, difficulty)
+  constructor(text, choices, answer, difficulty) {
+    this.text = text;
+    this.choices = choices;
+    this.answer = answer;
+    this.difficulty = difficulty;
+  }
 
-    // 2. shuffleChoices()
-    shuffleChoices() {
-        this.choices.sort(() => Math.random() - this.choices.length);
-    }
+  // 2. shuffleChoices()
+  shuffleChoices() {
+    this.choices.sort(() => Math.random() - this.choices.length);
+  }
 }
--- src/quiz.js
+++ src/quiz.js
@@ -1,15 +1,52 @@
 class Quiz {
-    // YOUR CODE HERE:
-    //
-    // 1. constructor (questions, timeLimit, timeRemaining)
+  // YOUR CODE HERE:
+  //
+  // 1. constructor (questions, timeLimit, timeRemaining)
+  constructor(questions, timeLimit, timeRemaining) {
+    this.questions = questions;
+    this.timeLimit = timeLimit;
+    this.timeRemaining = timeRemaining;
+    this.correctAnswers = 0;
+    this.currentQuestionIndex = 0;
+  }
 
-    // 2. getQuestion()
-    
-    // 3. moveToNextQuestion()
+  // 2. method getQuestion()
 
-    // 4. shuffleQuestions()
+  getQuestion() {
+    let currentQuestion = this.questions[this.currentQuestionIndex];
 
-    // 5. checkAnswer(answer)
+    return currentQuestion;
+  }
 
-    // 6. hasEnded()
+  // 3. moveToNextQuestion()
+
+  moveToNextQuestion() {
+    this.currentQuestionIndex++;
+  }
+
+  // 4. shuffleQuestions()
+
+  shuffleQuestions() {
+    this.questions.sort(() => Math.random() - this.questions.length);
+  }
+
+  // 5. checkAnswer(answer)
+
+  checkAnswer(answer) {
+
+
+    if (answer === currentQuestion.answer) {
+      this.correctAnswers++;
+    }
+  }  
+
+  // 6. hasEnded()
+
+  hasEnded() {
+    if (this.currentQuestionIndex < this.questions.length) {
+      return false;
+    } else if (this.currentQuestionIndex === this.questions.length) {
+      return true;
+    }
+  }
 }
